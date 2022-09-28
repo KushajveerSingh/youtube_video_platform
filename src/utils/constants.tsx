1634e51adb55de2837778790b5b2d768cc288c62
KushajveerSingh <kushajreal@gmail.com>
2022-09-28 05:40:16 -0400

    remove constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -15,7 +15,7 @@ import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon /> },
-  { name: 'JS Mastery', icon: <CodeIcon /> },
+  // { name: 'JS Mastery', icon: <CodeIcon /> },
   { name: 'Coding', icon: <CodeIcon /> },
   { name: 'ReactJS', icon: <CodeIcon /> },
   { name: 'NextJS', icon: <CodeIcon /> },
